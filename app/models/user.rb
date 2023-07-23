025855e494468804b10045b5ded1e1fc4d44a1f8
EC2 Default User <ec2-user@ip-172-31-20-101.ap-northeast-1.compute.internal>
2023-07-23 15:44:32 +0000

    [Add}応用課題2

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  has_one_attached :profile_image
+
+
+
+
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+end
